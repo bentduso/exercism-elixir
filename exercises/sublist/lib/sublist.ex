c4f60af580257128da25be0f696c01332c708c98
Benjamin Zubača <bzubaca123@gmail.com>
2024-05-15 10:46:20 +0200

    Complete 'Sublist' exercise

---
+++ exercises/sublist/lib/sublist.ex
@@ -0,0 +1,20 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare([], []), do: :equal
+  def compare(_, []), do: :superlist
+  def compare([], _), do: :sublist
+  def compare(a, a), do: :equal
+
+  def compare(a, b) when length(a) < length(b) do
+    if sublist?(a, b), do: :sublist, else: :unequal
+  end
+
+  def compare(a, b) do
+    if sublist?(b, a), do: :superlist, else: :unequal
+  end
+
+  defp sublist?(a, b), do: a in Enum.chunk_every(b, length(a), 1, :discard)
+end
