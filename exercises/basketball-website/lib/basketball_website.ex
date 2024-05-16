909fad77e925364db0f487db17710de8dd1eea38
Benjamin Zubača <bzubaca123@gmail.com>
2024-05-16 21:20:56 +0200

    Complete 'Basketball Website' exercise

---
+++ exercises/basketball-website/lib/basketball_website.ex
@@ -0,0 +1,26 @@
+defmodule BasketballWebsite do
+  # data = %{
+  #   "team_mascot" => %{
+  #     "animal" => "bear",
+  #     "actor" => %{
+  #       "first_name" => "Noel"
+  #     }
+  #   }
+  # }
+
+  def extract_from_path(data, path) do
+    keys = String.split(path, ".")
+    traverse(data, keys)
+  end
+
+  defp traverse(data, []), do: data
+
+  defp traverse(%{} = data, [key | tail]) when is_map_key(data, key),
+    do: traverse(data[key], tail)
+
+  defp traverse(_, _), do: nil
+
+  def get_in_path(data, path) do
+    get_in(data, String.split(path, "."))
+  end
+end
