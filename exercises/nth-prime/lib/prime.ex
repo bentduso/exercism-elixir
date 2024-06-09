ecd8c5733853738e7bf12ae24cf9a550bb370f70
Benjamin Zubača <bzubaca123@gmail.com>
2024-06-09 23:56:54 +0200

    Fix 'Nth Prime' exercise bugs

--- exercises/nth-prime/lib/prime.ex
+++ exercises/nth-prime/lib/prime.ex
@@ -3,19 +3,20 @@ defmodule Prime do
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
-  def nth(1), do: 2
+  def nth(0), do: raise(ArgumentError)
 
   def nth(count) do
-    Stream.iterate(3, &(&1 + 2))
-    |> Stream.filter(&prime?/1)
+    Stream.iterate(1, &(&1 + 1))
+    |> Stream.filter(&prime?(&1))
     |> Enum.at(count - 1)
   end
 
   @spec prime?(non_neg_integer) :: boolean
-  defp prime?(number) when number <= 1, do: false
+  def prime?(number) when number < 2, do: false
+  def prime?(2), do: true
 
-  defp prime?(number) do
-    2..floor(:math.sqrt(number))
+  def prime?(number) do
+    2..(trunc(:math.sqrt(number)) + 1)
     |> Enum.all?(&(rem(number, &1) != 0))
   end
 end
