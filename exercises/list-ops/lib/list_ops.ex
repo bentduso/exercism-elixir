de4e354ddc68042b595848ee537eb963f3eba0cb
Benjamin Zubača <bzubaca123@gmail.com>
2024-11-21 18:53:00 +0100

    Complete `List Ops` exercise

---
+++ exercises/list-ops/lib/list_ops.ex
@@ -0,0 +1,60 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+
+  @spec count(list) :: non_neg_integer
+  def count(l), do: do_count(l, 0)
+
+  @spec do_count(list, non_neg_integer) :: non_neg_integer
+  defp do_count([], acc), do: acc
+  defp do_count([_head | tail], acc), do: do_count(tail, acc + 1)
+
+  @spec reverse(list) :: list
+  def reverse(l), do: do_reverse(l, [])
+
+  @spec do_reverse(list, list) :: list
+  defp do_reverse([], acc), do: acc
+  defp do_reverse([head | tail], acc), do: do_reverse(tail, [head | acc])
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f), do: do_map(l, f, [])
+
+  @spec do_map(list, (any -> any), list) :: list
+  defp do_map([], _f, acc), do: reverse(acc)
+  defp do_map([head | tail], f, acc), do: do_map(tail, f, [f.(head) | acc])
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f), do: do_filter(l, f, [])
+
+  @spec do_filter(list, (any -> as_boolean(term)), list) :: list
+  defp do_filter([], _f, acc), do: reverse(acc)
+
+  defp do_filter([head | tail], f, acc) do
+    if f.(head) do
+      do_filter(tail, f, [head | acc])
+    else
+      do_filter(tail, f, acc)
+    end
+  end
+
+  @type acc :: any
+
+  @spec foldl(list, acc, (any, acc -> acc)) :: acc
+  def foldl([], acc, _f), do: acc
+  def foldl([head | tail], acc, f), do: foldl(tail, f.(head, acc), f)
+
+  @spec foldr(list, acc, (any, acc -> acc)) :: acc
+  def foldr([], acc, _f), do: acc
+  def foldr([head | tail], acc, f), do: f.(head, foldr(tail, acc, f))
+
+  @spec append(list, list) :: list
+  def append([], b), do: b
+  def append([head | tail], b), do: [head | append(tail, b)]
+
+  @spec concat([[any]]) :: [any]
+  def concat([]), do: []
+  def concat([head | tail]), do: append(head, concat(tail))
+end
